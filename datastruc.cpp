8c3ab52af0046991aa35dfba2655cbf6c2297e4b
Ingit <ingitag777@gmail.com>
2019-04-20 23:03:50 +0530

    menu created for stack in main function

--- datastruc.cpp
+++ datastruc.cpp
@@ -56,4 +56,36 @@ void showstack()
 		}		
 	}
 }
+int main()
+{
+	int ch,x;
+	Node *nptr;
+	
+	while(1)
+	{
+		cout<<"\n\n1.Push\n2.Pop\n3.Display\n4.Exit";
+		cout<<"\nEnter your choice(1-4):";
+		cin>>ch;
+		
+		switch(ch){
+			case 1: cout<<"\nEnter data:";
+					cin>>x;
+					nptr=newnode(x);
+					push(nptr);
+					break;
+			
+			case 2: pop();
+					break;
+					
+			case 3: showstack();
+					break;
+			
+		        case 4: exit(0);
+			
+			default: cout<<"\nWrong choice!!";
+		}
+	}
+	
+	return 0;
+}
  
